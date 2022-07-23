4fe326510e721ff30328d30366e1e42f28483f76
mostrovska <ostrovskamari97@gmail.com>
2022-07-23 14:55:24 +0300

    homework1

---
+++ homework1/js1.js
@@ -0,0 +1,83 @@
+let h1 = 'hello';
+console.log(h1);
+// alert(h1);
+document.write(h1);
+let h2 = 'owu';
+console.log(h2);
+// alert(h2);
+document.write(h2);
+let h3 = 'com';
+console.log(h3);
+// alert(h3);
+document.write(h3);
+let h4 = 'ua';
+console.log(h4);
+// alert(h4);
+document.write(h4);
+let a1 = 1;
+console.log(a1);
+// alert(a1);
+document.write(a1);
+let a2 = 10;
+console.log(a2);
+// alert(a2);
+document.write(a2);
+let a3 = -999;
+console.log(a3);
+// alert(a3);
+document.write(a3);
+let a4 = 123;
+console.log(a4);
+// alert(a4);
+document.write(a4);
+let a5 = 3.14;
+console.log(a5);
+// alert(a5);
+document.write(a5);
+let a6 = 2.7;
+console.log(a6);
+// alert(a6);
+document.write(a6);
+let a7 = 16;
+console.log(16);
+// alert(a7);
+document.write(a7);
+let b1 = true;
+console.log(b1);
+// alert(b1);
+document.write(b1);
+let b2 = false;
+console.log(b2);
+// alert(b2);
+document.write(b2);
+let firstName = 'Mariana ';
+console.log(firstName);
+let middleName = 'Volodymyrivna';
+console.log(middleName);
+let lastName = 'Ostrovska ';
+console.log(lastName);
+let person = (lastName + firstName + middleName);
+console.log(person);
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+let myName = prompt('name');
+console.log(myName);
+let myMiddleName = prompt('MiddleName');
+console.log(myMiddleName);
+let myAge = prompt('Age');
+console.log(myAge);
+
+
+
+
+
+
+
+
+
+
+
