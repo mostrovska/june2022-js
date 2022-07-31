02b9346ef5eeb2214f843fafed7a6b65406fa2ac
mostrovska <ostrovskamari97@gmail.com>
2022-07-31 12:43:29 +0300

    homework3

---
+++ homework3/answer.js
@@ -0,0 +1,149 @@
+// 1.Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'.
+//     Перевірте  скрипт при a, що дорівнює 1, 0, -3
+// let number=+prompt('x');
+// if (number !==0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 2. Дано змінну time яка рівна числу від 0 до 59.
+// Потрібно написати код, який перевірить, до якої четверті години
+// попадає число(в першу, другу, третю или четверту частину години).
+// let time=prompt('До якої четверті години попадає число 0-59');
+// if (time>=0 && time<=14){
+//     console.log('first');
+// }else if (time>=15 && time <=29){
+//     console.log('second');
+// }else if (time>=30 && time <=44){
+//     console.log('third');
+// }else if (time >=45 && time <=59){
+//     console.log('fourth');
+// }else {
+//     console.log('error');
+// }
+
+// 3. У змінній day дано якесь число від 1 до 31.
+// Потрібно визначити, у яку половину(декаду) місяця потрапляє це число
+// (у першу, другу чи третю).
+// let day=prompt('У яку декаду місяця потрапляє число 1-31');
+// if (day>=1 && day<=10){
+//     console.log('first');
+// }else if (day>=11 && day<=20){
+//     console.log('second');
+// }else if (day>=21 && day<=31){
+//     console.log('third')
+// }else {
+//     console.log('error')
+// }
+
+// 4.Скласти розклад на тиждень за домопоги switch.
+//     Користувач вводить порядковий номер дня тижня і на екрані
+//     відображається інфа що заплановано на цей день
+//     (можна замість плану на день, назву дня англійською).
+// let week=+prompt('Schedule of the week');
+// switch (week){
+//     case 1:
+//         console.log('Mondey');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thusday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     case 7:
+//         console.log('Sunday');
+//         break;
+// }
+
+// 5. Користувач вводить або має два числа.
+//     Потрібно знайти та вивести максимальне число з тих двох .
+//     Також потрібно врахувати коли введені рівні числа.
+// let user=+prompt('The maximum number');
+// switch (user){
+//     case 1:
+//         console.log('very little');
+//         break;
+//     case 2:
+//         console.log('small');
+//         break;
+//     case 3:
+//         console.log('not enought');
+//         break;
+//     case 4:
+//         console.log('some morre');
+//         break;
+//     case 5:
+//         console.log('exactly that');
+//         break;
+//     case 6:
+//         console.log('too much');
+//         break;
+//
+// }
+
+// let number=+prompt('The maximum number');
+// switch (number){
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log('small');
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log('almost that but a little more');
+//         break;
+//     case 7:
+//         console.log('yes it is 7');
+//         break;
+//     default:
+//         console.log('too much');
+// }
+
+// let number=+prompt('The maximum number');
+// if (number>=0 && number<=20){
+//     console.log('small');
+// }else if (number>=21 && number<=49){
+//     console.log('almost that');
+// }else if (number===50){
+//     console.log('yes, it is 50');
+// }else{
+//     console.log('too much');
+// }
+
+ // let number=+prompt('max-number');
+ // a=33;
+ // b=55;
+ // if (a<b){
+ //     console.log('min-number');
+ // }else if (b>a){
+ //     console.log('max-number');
+ // }else{
+ //     console.log('same numbers')
+ // }
+
+// 6. є змінна х, яка може прийняти будь-яке значення
+// (стрінг, число, undefined, null  і тд включно).
+// Напишіть код який, за допомоги  оператора || буде присвоювати
+// змінній х значення "default"  якщо значення змінної х являється
+// falsy (хибноподыбне, тобто кастується до false)
+
+// let d=2>4||'default';
+// console.log(d);
+
+
+
+
+
